3571b2cc8803eaafd02711c10cac8d01912f3d0b
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-05-10 09:23:14 +0200

    chore: Update base URL for socket connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ const enviroment = {
     prodution:true,
     state: "DEV",
     base_url_local: "https://app-radio-backend-git-main-angelacedo12s-projects.vercel.app/",
-    base_url_socket : "https://appradio-backend.onrender.com/",
+    base_url_socket : "https://appradio-backend.onrender.com:3001",
     MAP_BOX_TOKEN: "pk.eyJ1IjoiYW5nZWwxMjMxMjMxIiwiYSI6ImNsaTM2YnNiNjBld3QzcWxzZm5tcWpyengifQ.MhtYCIUTIf2AcAIGBZAYnA"
 }
 export default enviroment;
