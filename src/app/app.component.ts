1f14daac0c7851930f6b78ef70428654189db058
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-05-11 12:19:34 +0200

    refactor: Update PWA registration strategy to registerWhenStable:1000

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RadioBrowserApi, Station } from 'radio-browser-api';
 import { ReproductorServiceService } from './services/reproductor-service.service';
+import { start } from 'repl';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +15,17 @@ export class AppComponent {
 
   ngOnInit(): void {
     
+    setTimeout(() => {
 
-      }
+      this.installPWA();
+    }, 1000);
+  }
+  installPWA() {
+
+  
+
+
+  }
     
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,9 +29,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      
+      registrationStrategy: 'registerWhenStable:1000'
     })
   ],
   providers: [
