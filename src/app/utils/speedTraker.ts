94fa5516f25a6d0b5df03e4d4edd80869ae53adb
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-05-20 13:25:17 +0200

    document speedTraker

--- src/app/utils/speedTraker.ts
+++ src/app/utils/speedTraker.ts
@@ -2,9 +2,30 @@ import { BehaviorSubject, Observable } from "rxjs";
 import { Coords } from "../models/Coords";
 import { signal } from "@angular/core";
 
-// Esta clase gestion internamente la velocidad de un vehiculo en movimiento y la almacena en un array de coordenadas
-// para luego calcular la velocidad media en un determinado intervalo de tiempo
-// y compararla con un limite de velocidad establecido
+/**
+ * @class SpeedTraker
+ * @description
+ * Esta clase gestiona internamente la velocidad de un vehículo en movimiento y la almacena en un array de coordenadas
+ * para luego calcular la velocidad media en un determinado intervalo de tiempo
+ * y compararla con un límite de velocidad establecido
+ * @example
+ * const speedTraker = new SpeedTraker(120);
+ * speedTraker.breakSpeedLimit.subscribe((value) => {
+ *  if(value) {
+ *   console.log('Has superado el límite de velocidad')
+ * }
+ * })
+ *  
+ * @param speedLimit - Límite de velocidad en km/h
+ * @returns {void}
+ * @method calculateSpeed - Calcula la velocidad media en un intervalo de tiempo
+ * @method getIntervalCoord - Obtiene las coordenadas del vehículo en un intervalo de tiempo
+ * @method breakSpeedLimit - Observable que emite un valor booleano cuando se supera el límite de velocidad
+ * @property speed - Velocidad actual del vehículo
+ * @property coord - Array de coordenadas del vehículo
+ * @property speedLimit - Límite de velocidad en km/h
+ * 
+ */
 
 export class SpeedTraker  {
     speed = 0;
@@ -32,6 +53,13 @@ export class SpeedTraker  {
       
 
     }
+    /*
+    * @method getIntervalCoord
+    * @description
+    * Obtiene las coordenadas del vehículo en un intervalo de tiempo
+    * @returns {void}
+    *  
+    */
     async getIntervalCoord() {
 
             setInterval(() => {
@@ -43,6 +71,12 @@ export class SpeedTraker  {
             },500)
             
         }
+    /*
+    * @method calculateSpeed
+    * @description
+    * Calcula la velocidad media en un intervalo de tiempo
+    * @returns {number}
+    */
     async calculateSpeed() {
             
             let sumLon = 0;
