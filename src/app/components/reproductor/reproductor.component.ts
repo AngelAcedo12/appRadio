9a7ae223dbdb0c7599ba4bd6fe50af6ed7af23e5
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2025-05-20 20:50:16 +0200

    Refactor ReproductorComponent for improved readability and consistency

--- src/app/components/reproductor/reproductor.component.ts
+++ src/app/components/reproductor/reproductor.component.ts
@@ -1,4 +1,14 @@
-import { AfterViewInit, Component, computed, effect, Input, OnInit, signal, Signal, SimpleChanges } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  computed,
+  effect,
+  Input,
+  OnInit,
+  signal,
+  Signal,
+  SimpleChanges,
+} from '@angular/core';
 import { ReproductorServiceService } from '../../services/reproductor-service.service';
 import { Station } from 'radio-browser-api';
 import { Sign } from 'crypto';
@@ -9,89 +19,71 @@ import { LocationRadiosService } from '../../services/location-radios.service';
 @Component({
   selector: 'app-reproductor',
   templateUrl: './reproductor.component.html',
-  styleUrl: './reproductor.component.css'
+  styleUrl: './reproductor.component.css',
 })
 export class ReproductorComponent implements AfterViewInit {
+  @Input() isAtransmision: boolean = false;
+  modeCar = signal(false);
+  coords: Coords[] = [];
+  volumeBar = signal(false);
+  recomendations: Station[] = [];
 
-  @Input() isAtransmision : boolean = false
-  modeCar = signal(false)
-  coords : Coords[]= []
-  volumeBar = signal(false)
-  recomendations : Station[] = []
+  constructor(
+    public reproductorService: ReproductorServiceService,
+    public locationService: LocationRadiosService,
+    public notificationService: NotificationService
+  ) {}
 
-  
-
-  constructor(public reproductorService:ReproductorServiceService,
-    public locationService:LocationRadiosService,
-     public notificationService: NotificationService){
-
-
+  openBar() {
+    this.volumeBar()
+      ? document
+          .getElementById('volumeBar')
+          ?.classList.replace('openBar', 'closeBar')
+      : document
+          .getElementById('volumeBar')
+          ?.classList.replace('closeBar', 'openBar');
+    this.volumeBar.update(() => (this.volumeBar() ? false : true));
+  }
+  updateModeCar() {
+    this.modeCar()
+      ? this.modeCar.update(() => false)
+      : this.modeCar.update(() => true);
+  }
+  setModeCar(boolean: boolean) {
+    this.modeCar.update(() => boolean);
   }
 
-  openBar(){
-    this.volumeBar() ? document.getElementById("volumeBar")?.classList.replace("openBar","closeBar") : document.getElementById("volumeBar")?.classList.replace("closeBar","openBar")
-    this.volumeBar.update(() => this.volumeBar() ? false : true )
-  }
-  updateModeCar(){
-    this.modeCar() ? this.modeCar.update(() => false) : this.modeCar.update(() => true) 
-  }
-  setModeCar(boolean: boolean){
-    this.modeCar.update(() => boolean)
-  }
-  
   ngAfterViewInit(): void {
-      this.reproductorService.addListertToAudio()
-      if(this.reproductorService.actualStation!=undefined){
-      this.reproductorService.loadStationInLocalStorage()
-      this.asigningSuscriptionToBreakLimit()
-
-
-  }
-}
-
-  state : Signal<boolean> = signal(false)
-  stateOpen = signal(false)
-
-
-
-
-  setState(){
-    if(this.reproductorService.actualStation!=undefined && this.reproductorService.state()){
-      
-      document.getElementById("rep")?.classList.replace("desactive","active")
-    }else{
-      document.getElementById("rep")?.classList.replace("active","desactive")
+    this.reproductorService.addListertToAudio();
+    if (this.reproductorService.actualStation != undefined) {
+      this.reproductorService.loadStationInLocalStorage();
     }
   }
-  
-  play(){
-    this.reproductorService.resume()
-    document.getElementById("rep")?.classList.replace("desactive","active")
-  }
-  pause(){
-    this.reproductorService.pause()
-    document.getElementById("rep")?.classList.replace("active","desactive")
-  }
 
-  changeVolume(event : any){
-    this.reproductorService.changeVolume(event.target.value)
-  }
-  asigningSuscriptionToBreakLimit(){
-  
-    this.reproductorService.observableSpeedTraker.breakSpeedLimit.subscribe((value) => {
-      if(value && !this.modeCar()){
-      
-        this.modeCar.update(() => true)
-        this.notificationService.openSnackBar({
-          message: "Has superado el límite de velocidad",
-          duration: 2000,
-          closeMessage: "Cerrar"
-        })
-      }
-      
+  state: Signal<boolean> = signal(false);
+  stateOpen = signal(false);
 
-    })
-
+  setState() {
+    if (
+      this.reproductorService.actualStation != undefined &&
+      this.reproductorService.state()
+    ) {
+      document.getElementById('rep')?.classList.replace('desactive', 'active');
+    } else {
+      document.getElementById('rep')?.classList.replace('active', 'desactive');
+    }
   }
 
+  play() {
+    this.reproductorService.resume();
+    document.getElementById('rep')?.classList.replace('desactive', 'active');
+  }
+  pause() {
+    this.reproductorService.pause();
+    document.getElementById('rep')?.classList.replace('active', 'desactive');
+  }
+
+  changeVolume(event: any) {
+    this.reproductorService.changeVolume(event.target.value);
+  }
 }
